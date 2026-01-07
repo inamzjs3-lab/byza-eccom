01e8c945c107263469cf0defc5c121b6310bcf49
numan <laginumi@gmail.com>
2026-01-06 22:14:52 -0800

    created 2 tables through raw sql quarey

---
+++ EntityLayer/Buyer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    [Table("Buyers")]
+    public class Buyer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int UserId { get; set; }
+       
+    }
+}
