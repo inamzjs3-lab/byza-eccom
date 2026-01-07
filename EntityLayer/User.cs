e9d8e07c57a9379fb0a80aae1bd366d5fd0ffe94
inamkhan <inamzjs3@gmail.com>
2026-01-07 11:43:08 +0530

    i created one user table

---
+++ EntityLayer/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityLayer
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        [Required]
+        public string Mobile { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsDeleted { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsLocked { get; set; }
+        public string UserRole { get; set; }
+        public int LoginTries { get; set; }
+
+
+    }
+}
