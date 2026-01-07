2ca5295919471b95df9a88bacccf5a74ab57735e
numan <laginumi@gmail.com>
2026-01-06 23:33:46 -0800

    User Repository Contract

---
+++ DataAccessLayer/Contracts/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer;
+
+namespace DataAccessLayer.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<int> AddUser(User user);
+        Task<bool> UpdateUser(User user);
+        Task<bool> DeleteUser(int id);
+        Task<User> GetById(int id);
+        Task<List<User>> GetAll(int skip, int top, string search, string orderBy);
+    }
+}
