9f837feeedd41f9eebfdde009f0b13dbb9723bae
numan <laginumi@gmail.com>
2026-01-07 22:21:10 -0800

    created new Controllers/AuthController1.cs

---
+++ Byza/Controllers/AuthController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Byza.Controllers
+{
+    public class AuthController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
